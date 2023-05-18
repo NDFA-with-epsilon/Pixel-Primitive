f39631f76ae91cb8cf2532f29a836a234e5f3e5e
Shashank <kiwimojito3012@proton.me>
2023-05-18 20:22:08 +0530

    Refactor

---
+++ src/bin/main.rs
@@ -0,0 +1,44 @@
+//grid of 64 x 48 pixels
+//5 pixel wide circle in the center i.e r = 5
+//eq of circle : (x-32)^2 + (y-24)^2 = r^2
+// '.' for pixels inside and on the boundary of circle. rest pixels to be shown as 'x'
+
+use primitive_pixel::{Pixels, PointPixel, GRID_C, GRID_R};
+
+// need error handling on this; .unwrap() functionality or something
+fn render_circle(pixels: &mut Pixels, radius: usize) {
+    // C(x, y) = (GRID_R/2, GRID_C/2)
+    let mut coords: Vec<(usize, usize)> = Vec::new(); //coordinates to be crossed
+
+    let mut diff_x: usize = 0;
+    let mut diff_y: usize = 0;
+
+    for (i, row) in pixels.matrix.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            if GRID_R / 2 > i {
+                diff_x = GRID_R / 2 - i;
+            } else {
+                diff_x = i - GRID_R / 2;
+            }
+
+            if GRID_C / 2 > j {
+                diff_y = GRID_C / 2 - j;
+            } else {
+                diff_y = j - GRID_C / 2;
+            }
+
+            if diff_x * diff_x + diff_y * diff_y <= radius * radius {
+                coords.push((i, j));
+            }
+        }
+    }
+
+    pixels.switch_pixel(&coords, PointPixel::Cross);
+    pixels.render();
+}
+
+fn main() {
+    let mut grid: Pixels = Pixels::new();
+
+    render_circle(&mut grid, 5);
+}
---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+/*
+GRID_R: Horizontal length of the grid in which pixels are to be rendered
+GRID_C: Vertical length of the grid in which pixels are to be rendered
+*/
+
+pub const GRID_R: usize = 64;
+pub const GRID_C: usize = 48;
+
+/*
+@PointPixel:
+Defines the two values for a pixel that can be rendered
+*/
+
+pub enum PointPixel {
+    Dot,
+    Cross,
+}
+
+/*
+@Pixels:
+Defines a 2D array that stores u8 values: '.' (dot) and 'x' (cross)
+*/
+
+pub struct Pixels {
+    pub matrix: [[char; GRID_C]; GRID_R],
+}
+
+//need error handling on these methods
+impl Pixels {
+    pub fn new() -> Self {
+        Self {
+            matrix: [['.'; GRID_C]; GRID_R],
+        }
+    }
+
+    pub fn render(&self) {
+        for (_, row) in self.matrix.iter().enumerate() {
+            for (_, col) in row.iter().enumerate() {
+                print!("{} ", col);
+            }
+
+            println!();
+        }
+    }
+
+    //need error handling on these functions
+    pub fn cross_pixel(&mut self, coords: &Vec<(usize, usize)>) {
+        if self.matrix.len() == 0 {
+            println!("pixel grid in uninitialied");
+            return;
+        }
+
+        if coords.len() == 0 {
+            println!("Crossing the centre only...");
+            self.matrix[GRID_R / 2 - 1][GRID_C / 2 - 1] = 'x';
+            return;
+        }
+
+        self.matrix[GRID_R / 2 - 1][GRID_C / 2 - 1] = 'x';
+        for it in coords {
+            self.matrix[it.0 - 1][it.1 - 1] = 'x';
+        }
+    }
+
+    pub fn dot_pixel(&mut self, coords: &Vec<(usize, usize)>) {
+        if self.matrix.len() == 0 {
+            println!("pixel grid in uninitialied");
+            return;
+        }
+
+        if coords.len() == 0 {
+            return;
+        }
+
+        for it in coords {
+            self.matrix[it.0 - 1][it.1 - 1] = '.';
+        }
+    }
+
+    pub fn switch_pixel(&mut self, coords: &Vec<(usize, usize)>, pixel: PointPixel) {
+        match pixel {
+            PointPixel::Dot => self.dot_pixel(coords),
+            PointPixel::Cross => self.cross_pixel(coords),
+        }
+    }
+}
